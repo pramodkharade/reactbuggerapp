12b57d5fa0633423c7400583b828cde904016bdf
pramodkharade <kharade.pramod91@gmail.com>
2020-05-12 13:01:00 +0530

    added the order button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -30,6 +30,7 @@ const BuildControls = (props) => (
     removed ={() => props.ingredientRemoved(ctrl.type)}
     disabled={props.disabled[ctrl.type]}/>
   ))}
+  <button className={Classes.OrderButton} disabled={!props.purchasable}>ORDER NOW!</button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,9 +19,23 @@ class BurgerBuilder extends Component {
       salad: 0,
       cheese: 0,
       bacon: 0,
-      meat: 2
+      meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  };
+  updatePurchasableState = (ingredients) => {
+
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => {
+        return sum + el;
+      }, 0);
+    this.setState({
+      purchasable: sum > 0
+    });
   };
   addIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
@@ -37,6 +51,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurchasableState(updatedIngredients);
   };
   removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
@@ -55,6 +70,7 @@ class BurgerBuilder extends Component {
       totalPrice: newPrice,
       ingredients: updatedIngredients
     });
+    this.updatePurchasableState(updatedIngredients);
   };
   render() {
     const disabledInfo = {
@@ -69,6 +85,7 @@ class BurgerBuilder extends Component {
             <BuildControls ingredientAdded={this.addIngredientHandler}
       ingredientRemoved={this.removeIngredientHandler}
       disabled = {disabledInfo}
+      purchasable = {this.state.purchasable}
       price={this.state.totalPrice}/>
         </Auxiliary>
       );
