52fec7b38961b911b5a796052597eb6c64fe603e
pramodkharade <kharade.pramod91@gmail.com>
2020-05-12 15:03:56 +0530

    added custom Button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Classes from './Button.module.css'
+const Button = (props) => (
+
+  <button className={[Classes.Button, Classes[props.btnType]].join(' ')} onClick={props.clicked}>{props.children}</button>
+);
+
+export default Button;
