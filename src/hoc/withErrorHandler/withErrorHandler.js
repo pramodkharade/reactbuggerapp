65aa65c2ebdf34da1d89c045115d0106452323a6
pramodkharade <kharade.pramod91@gmail.com>
2020-05-13 20:03:43 +0530

    removed the interceptor once used to avoid memory leakage

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -7,18 +7,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({
           error: null
         });
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({
           error: error
         });
         console.log('Interceptor is::', error.message);
       });
+    }
+    // removing interceptor to avoid memory leakage 
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
     errorConfrimedHandler = () => {
       this.setState({
