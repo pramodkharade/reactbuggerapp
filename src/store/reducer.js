dc42a6f0eef5fa5a950f9394290d0157c5de9406
pramodkharade <kharade.pramod91@gmail.com>
2020-05-18 14:50:52 +0530

    reducer for ingredients

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,37 @@
 import * as actionType from './actions';
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    salad: 0,
+    cheese: 0,
+    meat: 0,
+    bacon: 0
+  },
   totalPrice: 0,
 };
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      };
+    case actionType.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      };
+    default:
+      return state;
+  }
 
 }
+
+export default reducer;
