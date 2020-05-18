26fe9b8fcf0545a3ffa218b923bc927cf52f0ce0
pramodkharade <kharade.pramod91@gmail.com>
2020-05-18 15:51:50 +0530

    order button issue solve with local state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,6 @@ class BurgerBuilder extends Component {
   //   this.state = {...}
   // }
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -43,9 +42,8 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({
-      purchasable: sum > 0
-    });
+    return sum > 0
+
   };
   // addIngredientHandler = (type) => {
   //   const oldCount = this.state.ingredients[type];
@@ -125,7 +123,7 @@ class BurgerBuilder extends Component {
       <BuildControls ingredientAdded={this.props.onIngredientAdded}
       ingredientRemoved={this.props.onIngredientRemoved}
       disabled = {disabledInfo}
-      purchasable = {this.state.purchasable}
+      purchasable = {this.updatePurchasableState(this.props.ings)}
       ordered={this.purchasingHandler}
       price={this.props.price}/>
    </Auxiliary>);
