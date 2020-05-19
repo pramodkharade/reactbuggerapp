10e5c6eb359523380a5851eeec04e07b9f4ab01c
pramodkharade <kharade.pramod91@gmail.com>
2020-05-19 14:09:37 +0530

    handle redirect issue if ingredients not loaded

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from '../Checkout/ContactData/ContactData';
@@ -13,18 +13,18 @@ class Checkout extends Component {
     this.props.history.goBack();
   };
   render() {
-    return (
-      <div>
-                <CheckoutSummary ingredients = {this.props.ings}
+    let summary = (<Redirect to='/'/>);
+    if (this.props.ings) {
+      summary = ( <div><CheckoutSummary ingredients = {this.props.ings}
       checkoutCancelled={this.checkoutCancelledHandler}
       checkoutContinued={this.checkoutContinuedHandler}/>
-       
-       <Route
+      <Route
       path={this.props.match.path + '/contact-data'}
       component={ContactData}
-      />
-            </div>
-      );
+      /> 
+      </div>)
+    }
+    return summary;
   }
 }
 const mapStateToProps = state => {
