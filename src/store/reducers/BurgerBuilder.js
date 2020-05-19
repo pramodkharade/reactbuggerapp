997223763ee990fd2e30dcb5a8f766c06e4ce9a7
pramodkharade <kharade.pramod91@gmail.com>
2020-05-19 16:52:05 +0530

    added unitiy to liean the reducer codes

--- src/store/reducers/BurgerBuilder.js
+++ src/store/reducers/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionType from '../actions/actionTypes';
+import { updateObject } from '../utility';
 const INGREDIENT_PRICE = {
   salad: 0.8,
   cheese: 0.9,
@@ -14,35 +15,35 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionType.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+      const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+      };
+      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+      const updatedState = {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICE[action.ingredientName]
-      };
+      }
+      return updateObject(state, updatedState);
     case actionType.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        },
-        totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName]
+      const updatedIng = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1
       };
+      const updatedIngs = updateObject(state.ingredients, updatedIng);
+      const updatedStates = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENT_PRICE[action.ingredientName]
+      }
+      return updateObject(state, updatedStates);
     case actionType.SET_INGREDIENTS:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: action.ingredients,
         totalPrice: 4,
         error: false
-      }
+      });
     case actionType.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
+      return updateObject(state, {
         error: true
-      }
+      })
     default:
       return state;
   }
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 const initialState = {
   orders: [],
   loading: false,
@@ -7,47 +8,39 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
+      return updateObject(state, {
         loading: true
-      }
+      });
     case actionTypes.PURCHASE_BURGER_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId,
-      };
-      return {
-        ...state,
+      const newOrder = updateObject(action.orderData, {
+        id: action.orderId
+      });
+      return updateObject(state, {
         loading: false,
-        orders: state.orders.concat(newOrder),
-        purchased: true
-      };
+        purchased: true,
+        orders: state.orders.concat(newOrder)
+      });
     case actionTypes.PURCHASE_BURGER_FAILED:
-      return {
-        ...state,
+      return updateObject(state, {
         loading: false
-      };
+      });
     case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
+      return updateObject(state, {
         purchased: false
-      }
+      });
     case actionTypes.FETCH_ORDER_START:
-      return {
-        ...state,
+      return updateObject(state, {
         loading: true
-      }
+      });
     case actionTypes.FETCH_ORDER_SUCCESS:
-      return {
-        ...state,
+      return updateObject(state, {
         orders: action.orders,
         loading: false
-      }
+      });
     case actionTypes.FETCH_ORDER_FAIL:
-      return {
-        ...state,
+      return updateObject(state, {
         loading: false
-      }
+      });
     default:
       return state;
   }
