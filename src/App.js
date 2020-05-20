bba89acce3dc94b52780f7b1800db575b5a88ab7
pramodkharade <kharade.pramod91@gmail.com>
2020-05-20 14:40:26 +0530

    logout implementation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Order/Orders';
 import { Route, Switch } from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Route path='/checkout' component={Checkout} />
         <Route path='/orders' component={Orders} />
         <Route path='/auth' component={Auth} />
+        <Route path='/logout' component={Logout} />
         <Route path='/' exact component={BurgerBuilder} />
         </Switch>
       </Layout>
---
+++ src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import * as authAction from '../../../store/actions/index';
+class Logout extends Component {
+  componentDidMount() {
+    this.props.onLogout();
+  }
+  render() {
+    return <Redirect to='/'/>;
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogout: () => dispatch(authAction.logout())
+  };
+};
+export default connect(null, mapDispatchToProps)(Logout);
